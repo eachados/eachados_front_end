95c0c17b46a0bc2a70d93640bfc7ffc3425a1dd1
Artur Cupelli <artur.cupelli@usp.br>
2022-10-03 16:50:03 -0300

    feat: Create Button atom

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { Button } from "../../components/atoms";
 
 const Splash: React.FC = () => {
-  return <h1>SPLASH</h1>;
+  return (
+    <div>
+      <h1>SPLASH</h1>
+      <Button
+        children="Teste"
+        onClick={() => console.log("[ON CLICK BUTTON] - Teste")}
+      />
+    </div>
+  );
 };
 
 export { Splash };
